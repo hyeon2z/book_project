170388098da06d9f8d6d5366eab4ef9c7fb4013e
limsol02 <yesshol@naver.com>
2023-10-25 17:38:18 +0900

    1025

---
+++ Book/src/sol/main.java
@@ -0,0 +1,19 @@
+package sol;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		// 로그인 연결하기
+		while(true) {
+			System.out.println("1.중고판매로 연결 / 2.종료");
+			String no = sc.nextLine();
+			if(no.equals("2")) {break;}
+			
+		}
+	}
+
+}
