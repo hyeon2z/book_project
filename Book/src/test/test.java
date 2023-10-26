a88dec8a3a90fb5cfd8cab32c89cfa4fad8f986f
limsol02 <yesshol@naver.com>
2023-10-26 22:14:47 +0900

    1026

---
+++ Book/src/test/test.java
@@ -0,0 +1,5 @@
+package test;
+
+public class test {
+	// 윤서 테스트
+}
---
+++ Book/src/vo/Book.java
@@ -0,0 +1,30 @@
+package vo;
+
+public class Book {
+	private String name;
+	private String company;
+	private String writer;
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+}
