19e664dd7798835dd3754606e2c1cf987f18f74b
hyeon2z <148454038+hyeon2z@users.noreply.github.com>
2023-10-30 01:13:10 +0900

    끝1

--- Book/src/myPage/MyPageMain.java
+++ Book/src/myPage/MyPageMain.java
@@ -25,16 +25,16 @@ public class MyPageMain { // 마이페이지
 			Scanner sc = new Scanner(System.in);	
 			System.out.print("\n이동할 목차를 선택하세요(0입력 시 종료) : ");
 			try {
-				int userChoice = Integer.parseInt(sc.nextLine())-1;
+				int userChoice = Integer.parseInt(sc.nextLine());
 				switch(userChoice) {
-					case -1:
+					case 0:
 						System.out.println("마이페이지 종료.");
 						break;
-					case 0:
+					case 1:
 						System.out.println("\n개인정보 수정 페이지 출력\n");
 						info.userInfoChange();
 						continue;
-					case 1:
+					case 2:
 						System.out.println("\n배송주소록 페이지 출력\n");
 						da.deliveryAddressAdd();
 						continue;
